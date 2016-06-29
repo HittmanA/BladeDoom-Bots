e78ac05e27917bc9fe9a450129dcc183f36e6bd3
HittmanA <hittmana@comcast.net>
2016-06-29 10:23:45 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -7,7 +7,10 @@ respondTo = function(input) {
       return "Hello " + chat.slice(-1).pop().user;
     
     if(input.match('what[^ ]* up') || input.match('sup') || input.match('how are you'))
-      return "Nothing, You?";
+      return "The sky...";
+    
+    if(input.match(input.match('how are you'))
+      return "I am a bot. I can only be good. How are you?";
     
     if(input.match('l(ol)+') || input.match('(ha)+(h|$)') || input.match('lmao')|| input.match('xD'))
       return "what's so funny?";
@@ -16,10 +19,10 @@ respondTo = function(input) {
       return "don't be such a negative nancy :(";
     
     if(input.match('(cya|bye|see ya|ttyl|talk to you later)'))
-      return ["alright, see you around", "good teamwork!"];
+      return "alright, see you around";
     
     if(input.match('(dumb|stupid|is that all)'))
-      return ["I would not be talk "+ chat.slice(-1).pop().user];
+      return ["I would not be talking "+ chat.slice(-1).pop().user];
     
     if(input == 'noop')
       return;
