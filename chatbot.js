0815d306d6d42417840c2521045022e72b98bd02
Hampton <herohamp@users.noreply.github.com>
2016-06-29 08:06:00 -0400

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,32 @@
+respondTo = function(input) {
+  
+    input = input.toLowerCase();
+    
+    if(input.match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)'))
+      return "Hello " + chat.slice(-1).pop().user;
+    
+    if(input.match('what[^ ]* up') || input.match('sup') || input.match('how are you'))
+      return "this github thing is pretty cool, huh?";
+    
+    if(input.match('l(ol)+') || input.match('(ha)+(h|$)') || input.match('lmao')|| input.match('xD'))
+      return "what's so funny?";
+    
+    if(input.match('^no+(\\s|!|\\.|$)'))
+      return "don't be such a negative nancy :(";
+    
+    if(input.match('(cya|bye|see ya|ttyl|talk to you later)'))
+      return ["alright, see you around", "good teamwork!"];
+    
+    if(input.match('(dumb|stupid|is that all)'))
+      return ["hey i'm just a proof of concept"];
+    
+    if(input == 'noop')
+      return;
+    
+    return input + " what?";
+  }
+  function gotMessage() {
+    if (chat.slice(-1).pop().user !== "bot") {
+      socket.emit('newMessage',respondTo(chat.slice(-1).pop().message));
+    }
+    }
